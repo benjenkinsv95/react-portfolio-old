c922ec03d2e8f4b3deb4ff4410925938bb45fc45
benjenkinsv95 <benjenkinsv95@gmail.com>
2021-03-27 22:36:44 -0400

    Adjusting switch to all

--- src/component/Portfolio/ItemGrid.js
+++ src/component/Portfolio/ItemGrid.js
@@ -7,22 +7,24 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 class ItemGrid extends Component {
 
     state = {
-        activeItem: '*',
+        activeItem: 'other',
     }
 
     componentDidMount() {
         var imgLoad = new ImagesLoaded('.grid');
-    
+
         imgLoad.on('progress', function(instance, image) {
             this.iso = new Isotope('.grid', {
                 itemSelector: '.grid-item',
                 layoutMode: "masonry"
             });
-        }); 
-        
+
+
+        });
+        setTimeout(() => this.onFilterChange("*"), 500)
     }
     onFilterChange = (newFilter) => {
-        
+
         this.setState({activeItem: newFilter});
         if (this.iso === undefined) {
             this.iso = new Isotope('.grid', {
@@ -30,9 +32,9 @@ class ItemGrid extends Component {
             layoutMode: "masonry"
             });
         }
-    
+
     // this.iso.arrange({ filter: newFilter });
-        
+
       if(newFilter === '*') {
         this.iso.arrange({ filter: `*` });
       } else {
@@ -49,12 +51,14 @@ class ItemGrid extends Component {
                 <li className={`${this.onActive('web')}`} data-wow-delay="0.3s" data-filter="web" onClick={() => {this.onFilterChange("web")}}>Web Development</li>
                 <li className={`${this.onActive(`extension`)}`} data-wow-delay="0.6s" data-filter="extension" onClick={()=> {this.onFilterChange("extension")}}>Chrome Extension</li>
                 <li className={`${this.onActive(`other`)}`} data-wow-delay="0.8s" data-filter="other" onClick={()=> {this.onFilterChange("other")}}>Other</li>
-            </ul>	
+            </ul>
 
             <div className="grid row">
                 <div className="col-md-3 col-sm-6 col-xs-12 grid-item web">
                     <div className="portfolio hover-style">
-                        <img src={require('../../image/portfolio/kanban-small.gif')} alt=""/>
+                        <video autoPlay loop muted playsInline>
+                          <source src={require('../../image/portfolio/kanban-small.mp4')} type="video/mp4"/>
+                        </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">React Kanban Board</h6>
@@ -65,12 +69,14 @@ class ItemGrid extends Component {
                                     <a href="https://benjenkinsv95.github.io/react-kanban-board/"><i className="icon-desktop"></i></a>
                                 </div>
                             </div>
-                        </div>						
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-3 col-sm-6 col-xs-12 grid-item web extension">
                     <div className="portfolio hover-style">
-                    <img src={require('../../image/portfolio/need-that-small.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                          <source src={require('../../image/portfolio/need-that-small.mp4')} type="video/mp4"/>
+                        </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">Do You Really Need That?</h6>
@@ -81,11 +87,13 @@ class ItemGrid extends Component {
                                 </div>
                             </div>
                         </div>
-                    </div>						
+                    </div>
                 </div>
                 <div className="col-md-6 col-sm-6 col-xs-12 grid-item other">
                     <div className="portfolio hover-style">
-                    <img src={require('../../image/portfolio/barliman-large.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                      <source src={require('../../image/portfolio/barliman-large.mp4')} type="video/mp4"/>
+                    </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">Barliman iOS</h6>
@@ -100,7 +108,9 @@ class ItemGrid extends Component {
                 </div>
                 <div className="col-md-6 col-sm-6 col-xs-12 grid-item other">
                     <div className="portfolio hover-style">
-                        <img src={require('../../image/portfolio/zelda-large.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                      <source src={require('../../image/portfolio/zelda-large.mp4')} type="video/mp4"/>
+                    </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">Legend of 50 RPG</h6>
@@ -110,12 +120,14 @@ class ItemGrid extends Component {
                                     </a>
                                 </div>
                             </div>
-                        </div>	
+                        </div>
                     </div>
                 </div>
                 <div className="col-md-3 col-sm-6 col-xs-12 grid-item web extension">
                     <div className="portfolio hover-style">
-                        <img src={require('../../image/portfolio/sentimentsion-small.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                      <source src={require('../../image/portfolio/sentimentsion-small.mp4')} type="video/mp4"/>
+                    </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">Sentimentsion</h6>
@@ -130,7 +142,9 @@ class ItemGrid extends Component {
                 </div>
                 <div className="col-md-3 col-sm-6 col-xs-12 grid-item other">
                     <div className="portfolio hover-style">
-                        <img src={require('../../image/portfolio/cs-history-small.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                      <source src={require('../../image/portfolio/cs-history-small.mp4')} type="video/mp4"/>
+                    </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">CS History Backgrounds</h6>
@@ -145,7 +159,9 @@ class ItemGrid extends Component {
                 </div>
                 <div className="col-md-3 col-sm-6 col-xs-12 grid-item other">
                     <div className="portfolio hover-style">
-                        <img src={require('../../image/portfolio/bitcube-small.gif')} alt=""/>
+                    <video autoPlay loop muted playsInline>
+                      <source src={require('../../image/portfolio/bitcube-small.mp4')} type="video/mp4"/>
+                    </video>
                         <div className="item-img-overlay">
                             <div className="overlay-info text-center">
                                 <h6 className="sm-titl">Bitcube Image Uploader</h6>
